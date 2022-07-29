773d717f0af77bcd8c19f03d3e78ce7f8f9f1f12
Lare Fadhil <91841151+larefazl02@users.noreply.github.com>
2022-07-30 00:50:13 +0300

    Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,97 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div id="app">
+    <gmap-map
+      :center="center"
+      :zoom="18"
+      map-style-id="roadmap"
+      :options="mapOptions"
+      style="width: 100vmin; height: 50vmin"
+      ref="mapRef"
+      @click="handleMapClick"
+    >
+      <GmapMarker
+        :position="marker.position"
+        :clickable="true"
+        :draggable="true"
+        @drag="handleMarkerDrag"
+        @click="panToMarker"
+      />
+    </gmap-map>
+    <button @click="geolocate">Detect Location</button>
+
+    <p>Selected Position: {{ marker.position }}</p>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  name: "App",
+  data() {
+    return {
+      marker: { position: { lat: 10, lng: 10 } },
+      center: { lat: 10, lng: 10 },
+      mapOptions: {
+        disableDefaultUI: true,
+      },
+    };
+  },
+  mounted() {
+    this.geolocate();
+    
+  },
+  methods: {
+    fetchData() {
+      if (!("geolocation" in navigator)) {
+        this.errorStr = "Geolocation is not available.";
+        return;
+      }
+      this.gettingLocation = true;
+      navigator.geolocation.watchPosition(
+        pos => {
+          this.gettingLocation = false;
+          this.initialPosition.lat = pos.coords.latitude;
+          this.initialPosition.lng = pos.coords.longitude;
+          const userData = {
+            position: {
+              lat: pos.coords.latitude,
+              lng: pos.coords.longitude
+            },
+            userName: this.usersName
+          };
+          this.userlocation = userData;
+          this.updateRoom(userData);
+        },
+        err => {
+          this.gettingLocation = false;
+          this.errorStr = err.message;
+        }
+      );
+    },
+    //detects location from browser
+    geolocate() {
+      navigator.geolocation.getCurrentPosition((position) => {
+        this.marker.position = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        };
+        this.panToMarker();
+      });
+    },
+    //sets the position of marker when dragged
+    handleMarkerDrag(e) {
+      this.marker.position = { lat: e.latLng.lat(), lng: e.latLng.lng() };
+    },
+    //Moves the map view port to marker
+    panToMarker() {
+      this.$refs.mapRef.panTo(this.marker.position);
+      this.$refs.mapRef.setZoom(18);
+    },
+    //Moves the marker to click position on the map
+    handleMapClick(e) {
+      this.marker.position = { lat: e.latLng.lat(), lng: e.latLng.lng() };
+      console.log(e);
+    },
+  },
+  
+};
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
